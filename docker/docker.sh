edecc6db472ce70d3328e31e7273411363c2d904
Sandra <sandra@websome.ch>
2015-12-08 07:48:08 +0100

    Fixed docker container problems

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,2 +1,4 @@
+docker stop puzzle-app
+docker rm puzzle-app
 docker run -d -v ~/word-puzzle/:/var/puzzle -p 80 --name=puzzle-app puzzle /bin/bash -c /var/puzzle/docker/startup.sh
 
--- docker/startup.sh
+++ docker/startup.sh
@@ -1,5 +1,7 @@
 mkdir /var/mongodb/data -p
+mkdir /var/puzzle/docker/log
 mongod --fork --logpath /var/puzzle/docker/log/mongodb.log --dbpath=/var/mongodb/data --smallfiles --noprealloc
+rm /var/puzzle/dist -R -f
 npm install
 export PATH="$(npm bin):$PATH"
 gulp build
